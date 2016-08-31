59055259ad44f615b35a83178ac75c82f3104606
Raizetronn <reeh.ortiz@gmail.com>
2016-08-31 12:56:11 -0300

    Add files via upload

---
+++ application/models/Categoria.php
@@ -0,0 +1,32 @@
+<?php
+class Application_Model_Categoria {
+    public function apagar($idcategoria) {
+        $tabPratos = new Application_Model_DbTable_Prato();
+        $prato = $tabPratos->fetchRow("idcategoria = $idcategoria");
+        
+        if($prato !== null){
+            throw new Exception("Categoria com Prato", 1);            
+        }
+        $tab = new Application_Model_DbTable_Categoria();
+        $tab->delete("idcategoria = $idcategoria");
+        return true;
+    }
+    public function atualizar(Application_Model_Vo_Categoria $categoria) {
+        $tab = new Application_Model_DbTable_Categoria();
+        $tab->update(array(
+           'categoria' => $categoria->getCategoria() 
+        ), 'idcategoria = '.$categoria->getIdcategoria());
+        
+        return true;
+    }
+    public function salvar(Application_Model_Vo_Categoria $categoria) {
+        $tab = new Application_Model_DbTable_Categoria();
+        $tab->insert(array(
+           'categoria' => $categoria->getCategoria() 
+        ));
+        
+       $id = $tab->getAdapter()->lastInsertId();
+       $categoria->setIdcategoria($id);
+       return true;
+    }
+}
